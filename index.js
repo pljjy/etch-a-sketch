fdab5a3bdafe723ce5775f8270181b328b6ecc78
pljjy <jotaro5jostar@gmail.com>
2023-09-06 15:45:21 -0400

    all basic functionality done did all the css and js that was needed, just some unnecesary details left

---
+++ index.js
@@ -0,0 +1,83 @@
+const main = document.querySelector(".main");
+const btn = document.querySelector("#change-state");
+const inputLength = document.querySelector("#grid-size");
+const reset = document.querySelector("#reset");
+
+// init
+btn.textContent = "hover";
+var state = "hover";
+var mousedown;
+document.body.onmousedown = () => mousedown = true;
+document.body.onmouseup = () => mousedown = false;
+changeMode(state, true);
+
+
+// eventListeners
+inputLength.addEventListener("input", () => {
+  if (parseInt(inputLength.value) > parseInt(inputLength.getAttribute("max"))) {
+    inputLength.value = parseInt(inputLength.getAttribute("max"));
+  } else if (parseInt(inputLength.value) < 0) {
+    inputLength.value = 1;
+  }
+
+  main.innerHTML = "";  
+  for (let x = 0; x < inputLength.value * inputLength.value; x++) {
+    main.appendChild(document.createElement("div"));
+  }
+
+  main.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${inputLength.value}, 1fr)`
+  );
+
+  changeMode(state, true)
+});
+
+btn.addEventListener("click", () => {
+  if (btn.textContent == "hover") {
+    btn.textContent = "click";
+    state = "click";
+  } else {
+    btn.textContent = "hover";
+    click = "hover";
+  }
+
+  changeMode(state)
+});
+
+reset.addEventListener("click", () => {
+  Array.from(main.children).forEach((element) => {
+    element.classList.remove("black-filled");
+  });
+});
+
+// functions
+function changeMode(newMode, fresh = false) {
+  if(!fresh){
+      Array.from(main.children).forEach((element) => {
+        main.removeChild(element);
+        main.appendChild(document.createElement("div"));
+      });
+  }
+
+  if(newMode == "hover"){
+    Array.from(main.children).forEach((element) => {
+        element.addEventListener("mouseenter", () => {
+          element.classList.add("black-filled");
+        });
+      });
+  }
+  else{
+    Array.from(main.children).forEach((element) => {
+        element.addEventListener("mouseenter", () => {
+          if(mousedown){
+            element.classList.add("black-filled");
+          }
+        });
+        
+        element.addEventListener("mousedown", ()=>{
+          element.classList.add("black-filled");
+        })
+      });
+  }
+}
